f4a069446c967aefc293881a87e7cdca04006557
Ángel Cruz García <angel.cruz@iactiva.ai>
2025-02-12 12:28:40 -0600

    deploy

--- src/components/chatbot/ChatBot.tsx
+++ src/components/chatbot/ChatBot.tsx
@@ -7,8 +7,8 @@ import UserForm from "./UserForm";
 const Chatbot: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
     // Establecer la URL del WebSocket
-    const { messages, isConnected, sendMessage} = useWebSocket("ws://localhost:5000/ws");  // Local
-    // const { messages, isConnected, sendMessage } = useWebSocket("wss://chatbot-go-dessa-production.up.railway.app/ws"); // Production 
+    // const { messages, isConnected, sendMessage} = useWebSocket("ws://localhost:5000/ws");  // Local
+    const { messages, isConnected, sendMessage } = useWebSocket("wss://chatbot-go-ramiro-production.up.railway.app/ws"); // Production 
 
     const [isChatVisible, setIsChatVisible] = useState(false);
 
